86b5323ff4507aa5b39105eeffee758b0f0d409b
diquesg <diquesg@gmail.com>
2025-07-18 19:00:55 -0300

    minor fixes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['upload.wikimedia.org', "i.scdn.co", "i.discogs.com", "m.media-amazon.com"]
+    domains: ['upload.wikimedia.org', "i.scdn.co", "i.discogs.com", "m.media-amazon.com", "cdn-images.dzcdn.net", "universalmusic.vtexassets.com", "blogger.googleusercontent.com"]
   },
   devIndicators: false,
 };
