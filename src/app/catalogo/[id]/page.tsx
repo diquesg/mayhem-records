e8eabb315eb3fa38c2ead15f7d55fd52c11b4502
diquesg <diquesg@gmail.com>
2025-08-06 18:51:19 -0300

    minor fix: Adjust product card price display

--- src/app/catalogo/[id]/page.tsx
+++ src/app/catalogo/[id]/page.tsx
@@ -283,9 +283,9 @@ export default function ProductPage() {
                 }
             
             <div className='fixed bottom-21 bg-[#131313] border-t border-t-neutral-600 rounded-t-2xl items-center h-21 w-full z-20 p-5 flex justify-between md:hidden'>
-                <p className='text-3xl font-bold text-white'>R$ {product.price.toFixed(2)}</p>
-                <button className='bg-blue-600 p-3 truncate rounded-full text-white font-bold flex gap-3 px-4 shadow-md shadow-neutral-950 active:scale-95 active:bg-blue-800' onClick={handleAddToCart}>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <p className='text-2xl min-w-34 sm:min-w-0 sm:text-3xl font-bold text-white'>R$ {product.price.toFixed(2)}</p>
+                <button className='bg-blue-600 p-3 truncate rounded-full text-white font-bold flex gap-3 px-2 items-center text-sm sm:text-lg sm:px-4 shadow-md shadow-neutral-950 active:scale-95 active:bg-blue-800' onClick={handleAddToCart}>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className='min-w-4'>
                         <path d="M1 1.5H5L7.68 14.89C7.77144 15.3504 8.02191 15.764 8.38755 16.0583C8.75318 16.3526 9.2107 16.509 9.68 16.5H19.4C19.8693 16.509 20.3268 16.3526 20.6925 16.0583C21.0581 15.764 21.3086 15.3504 21.4 14.89L23 6.5H6M10 21.5C10 22.0523 9.55228 22.5 9 22.5C8.44772 22.5 8 22.0523 8 21.5C8 20.9477 8.44772 20.5 9 20.5C9.55228 20.5 10 20.9477 10 21.5ZM21 21.5C21 22.0523 20.5523 22.5 20 22.5C19.4477 22.5 19 22.0523 19 21.5C19 20.9477 19.4477 20.5 20 20.5C20.5523 20.5 21 20.9477 21 21.5Z" 
                             stroke="white" 
                             strokeWidth="2" 
